21bee567e4f3f3a59e8611da94c1b4c1110fe30c
John Brechon <john.brechon@allscripts.com>
2021-11-26 16:04:42 -0600

    Updates to object, and over under line.

--- NbaGames.py
+++ NbaGames.py
@@ -1,9 +1,7 @@
 # This is a file with classes that conform to the json returned from this endpoint: http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard
-
+from pprint import pprint
 class NbaGames():
-	def __init__(self, day, events, leagues, season):
-		self.day = Day(**day) # This is a Day data Type
-
+	def __init__(self, events, leagues, season, day=None, week=None):
 		eventCollection = []
 		for event in events:
 			eventCollection.append(Event(**event))
@@ -15,37 +13,62 @@ class NbaGames():
 		self.leagues = leagueCollection  # Collection of League
 
 		self.season = Season(**season) # Season Object with type and year only
+		
+		if day is not None:
+			self.day = Day(**day)
+			
+		if week is not None:
+			self.week = week
 
 class Address():
-	def __init__(self, city, state):
+	def __init__(self, city, state=None):
 		self.city = city # String 'Portland'
-		self.state = state # String e.g. 'OR'
+		if state is not None:
+			self.state = state # String e.g. 'OR'
 
 class Athlete():
-	def __init__(self, displayName, fullName, headshot, id, jersey, links, position, shortName, team, active=None):
+	def __init__(self, displayName, fullName, id, links, position, shortName, team, period=None, active=None, jersey=None, headshot=None, playerId=None):
 		self.active = active # Bool
 		self.displayName = displayName # Two part String 'Justin ', 'Holiday'
 		self.fullName = fullName # Two part String 'Justin ' 'Holiday'
 		self.headshot = headshot # This is a link to the headshot of each player
 		self.id = id # String (e.g. '2284101' - Should return Justin Holiday)
-		self.jersey = jersey # String rep. of a number (e.g. '8')
+		if jersey is not None:
+			self.jersey = jersey # String rep. of a number (e.g. '8')
 
-		linkCollection = []
-		for link in links:
-			linkCollection.append(Link(**link))
-		self.links = linkCollection # Collection of Links (href and rel only)
+		if links is not None:
+			self.links = links
+#		linkCollection = []
+#		for link in links:
+#			linkCollection.append(Link(**link))
+#		self.links = linkCollection # Collection of Links (href and rel only)
 
-		self.position = Position(**position) # Position Object
+		if playerId is not None:
+			self.playerId = playerId
+		if period is not None:
+			self.period = period
+		if position is not None:
+			self.position = position
+#		self.position = Position(**position) # Position Object
 		self.shortName = shortName # Two Part String - (e.g. 'J. ' 'Holiday')
-		self.team = Team(**team) # Team Object with Id only
+		if team is not None:
+			self.team = team
+#		self.team = Team(**team) # Team Object with Id only
 
+class Batter():
+	def __init__(self, athlete, period, playerId, summary):
+		self.athlete = Athlete(*athlete)
+		self.period = period
+		self.playerId = playerId
+		self.summary = summary
+		
 class Broadcast():
 	def __init__(self, market, names):
 		self.market = market # String representing (National, Home, Away)
 		self.names = names # Collection of strings representing channel (e.g. 'FS1', 'NBATV')
 
 class Competition():
-	def __init__(self, attendance, broadcasts, competitors, conferenceCompetition, date, geoBroadcasts, id, neutralSite, notes, recent, series, startDate, status, timeValid, type, uid, venue, headlines=None, odds=None, situation=None, tickets=None):
+	def __init__(self, attendance, broadcasts, competitors, conferenceCompetition, date, geoBroadcasts, id, neutralSite, notes, recent, startDate, status, timeValid, type, uid, venue, headlines=None, leaders=None, odds=None, situation=None, tickets=None,  series=None, wasSuspended=None):
 		self.attendance = attendance # Int
 
 		broadcastCollection = []
@@ -84,7 +107,9 @@ class Competition():
 			
 		self.recent = recent # Bool
 		
-		self.series = series
+		if series is not None:
+			self.series = series
+			
 		if situation is not None:
 			self.situation = Situation(**situation) # Situation Object
 			
@@ -99,6 +124,13 @@ class Competition():
 		self.type = Competition_Type(**type) # CompetitionType object
 		self.uid = uid # String of userId 's:40~l:46~e:401267427~c:401267427'
 		self.venue = Venue(**venue) # Venue Object
+		
+		if wasSuspended is not None:
+			self.wasSuspended = wasSuspended
+			
+		
+		if leaders is not None:
+			self.leaders = leaders
 
 class Competition_Type():
 	def __init__(self, abbreviation, id):
@@ -106,11 +138,12 @@ class Competition_Type():
 		self.id = id # String of Int e.g. '1'
 
 class Competitor():
-	def __init__(self, homeAway, id, order, records, score, statistics, team, type, uid, leaders=None, linescores=None, winner=None):
+	def __init__(self, homeAway, id, order, records, score, statistics, team, type, uid, errors=None, hits=None, leaders=None, linescores=None, probables=None, winner=None):
 		self.homeAway = homeAway # String that will be 'home' or 'away'
 		self.id = id # String representing id as number
 
 		if leaders is not None:
+#			print("Leader: {}".format(leaders))
 			leaderCollection = []
 			for leader in leaders:
 				leaderCollection.append(Leader(**leader))
@@ -140,6 +173,15 @@ class Competitor():
 		self.type = type # String (e.g. 'team')
 		self.uid = uid # String for userId I assume ( e.g. 's:40~l:46~t:11')
 		self.winner = winner # Bool
+		
+		if errors is not None:
+			self.errors = errors
+			
+		if hits is not None:
+			self.hits = hits
+			
+		if probables is not None:
+			self.probables = probables
 
 class Day():
 	def __init__(self, date):
@@ -151,7 +193,7 @@ class DeviceRestrictions():
 		self.type = type # String e.g. 'whitelist'
 		
 class Event():
-	def __init__(self, competitions, date, id, links, name, season, shortName, status, uid):
+	def __init__(self, competitions, date, id, links, name, season, shortName, status, uid, weather=None):
 		competitionCollection = []
 		for competition in competitions:
 			competitionCollection.append(Competition(**competition))
@@ -170,6 +212,9 @@ class Event():
 		self.shortName = shortName # String e.g. 'OKC @ POR'
 		self.status = Status(**status) # Status Object
 		self.uid = uid # UserId I assume - String
+		
+		if weather is not None:
+			self.weather = weather
 
 class GeoBroadcast():
 	def __init__(self, lang, market, media, region, type):
@@ -201,7 +246,7 @@ class Headline():
 		self.video = videoCollection # Collection of Video Objects
 		
 class LastPlay():
-	def __init__(self, id, probability, scoreValue, text, type, athletesInvolved=None, team=None):
+	def __init__(self, id, scoreValue, type, alternativePlay=None, alternativeType=None, atBatId=None, athletesInvolved=None, probability=None, summaryType=None, team=None, text=None):
 	
 		if athletesInvolved is not None:
 			athleteCollection = []
@@ -210,17 +255,38 @@ class LastPlay():
 			self.athletesInvolved = athleteCollection
 			
 		self.id = id # String rep of Id '401267427323'
-		self.probability = Probability(**probability) # Probability Object
+		if probability is not None:
+			self.probability = Probability(**probability) # Probability Object
 		self.scoreValue = scoreValue # Int of Score
-		self.text = text # String two part - 'End of the ' '2nd Quarter'
+		if text is not None:
+			self.text = text # String two part - 'End of the ' '2nd Quarter'
 		self.type = LastPlay_Type(**type) # Type Object (id, text)
 		if team is not None:
 			self.team = Team(**team)
+			
+		if alternativeType is not None:
+			self.alternativeType = alternativeType
+		
+		if alternativePlay is not None:
+			self.alternativePlay = alternativePlay
+		
+		if atBatId is not None:
+			self.atBatId = atBatId
 
 class LastPlay_Type():
-	def __init__(self, id, text):
+	def __init__(self, id, text, type=None, alternativeText=None, abbreviation=None):
 		self.id = id # String e.g. '412'
 		self.text = text # Two part 'End ' 'Period'
+		
+		if abbreviation is not None:
+			self.abbreviation = abbreviation
+			
+		if type is not None:
+			self.type = type
+			
+		if alternativeText is not None:
+			self.alternativeText = alternativeText
+		
 
 class Leader():
 	def __init__(self, abbreviation, displayName, leaders, name, shortDisplayName):
@@ -236,7 +302,7 @@ class Leader():
 		self.shortDisplayName = shortDisplayName # This is the short displayName (e.g. 'Pts')
 
 class League():
-	def __init__(self, abbreviation, calendar, calendarEndDate, calendarIsWhitelist, calendarStartDate, calendarType, id, name, season, slug, uid):
+	def __init__(self, abbreviation, calendar, calendarEndDate, calendarIsWhitelist, calendarStartDate, calendarType, id, name, season, slug, uid, midsizeName=None):
 		self.abbreviation = abbreviation # String e.g. 'NBA'
 		self.calendar = calendar # Collection of String Dates e.g '2020-12-11T08:00Z'
 		self.calendarEndDate = calendarEndDate # String of Date e.g. '2020-12-11T08:00Z'
@@ -248,6 +314,9 @@ class League():
 		self.season = League_Season(**season) # Season Object
 		self.slug = slug # String e.g. 'nba' - Not sure what this represents
 		self.uid = uid # String of id e.g. 's:40~l:46'
+		
+		if midsizeName is not None:
+			self.midsizeName = midsizeName
 
 class League_Season():
 	def __init__(self, endDate, startDate, type, year):
@@ -280,11 +349,16 @@ class Media():
 		self.shortName = shortName # ShortName of Media (e.g. 'NBATV')
 
 class Odd():
-	def __init__(self, details, overUnder, provider):
+	def __init__(self, details, overUnder, provider, awayTeamOdds=None, homeTeamOdds=None):
 		self.details = details	# details string (e.g. 'POR -5.5')
 		self.overUnder = overUnder # Float (e.g. 224.5)
 		self.provider = Provider(**provider)
 
+		if awayTeamOdds is not None:
+			self.awayTeamOdds = awayTeamOdds
+			
+		if homeTeamOdds is not None:
+			self.homeTeamOdds = homeTeamOdds
 
 class Position():
 	def __init__(self, abbreviation):
@@ -323,8 +397,35 @@ class Season_Type():
 		self.type = type # Int e.g. 2
 
 class Situation():
-	def __init__(self, lastPlay):
+	def __init__(self, lastPlay, batter=None, balls=None, dueUp=None, strikes=None, onFirst=None, onSecond=None, onThird=None, outs=None, pitcher=None):
 		self.lastPlay = LastPlay(**lastPlay)
+		
+		if batter is not None:
+			self.batter = Batter(**batter)
+		
+		if dueUp is not None:
+			self.dueUp = dueUp
+			
+		if pitcher is not None:
+			self.pitcher = pitcher
+			
+		if balls is not None:
+			self.balls = balls
+			
+		if strikes is not None:
+			self.strikes = strikes
+			
+		if onFirst is not None:
+			self.onFirst = onFirst
+			
+		if onSecond is not None:
+			self.onSecond = onSecond
+			
+		if onThird is not None:
+			self.onThird = onThird
+			
+		if outs is not None:
+			self.outs = outs
 
 class Statistic():
 	def __init__(self, abbreviation, displayValue, name, rankDisplayValue=None):
@@ -348,7 +449,7 @@ class Status():
 		self.type = Status_Type(**type) # StatusType object
 
 class Status_Type():
-	def __init__(self, completed, description, detail, id, name, shortDetail, state):
+	def __init__(self, completed, description, detail, id, name, shortDetail, state, altDetail=None):
 		self.completed = completed # Bool
 		self.description = description # String (e.g. 'Schedule', 'Halftime')
 		self.detail = detail # Multi-line String (e.g. 'Mon, January ' '25th at 10:00 PM' 'EST', or one line 'Halftime')
@@ -356,6 +457,9 @@ class Status_Type():
 		self.name = name	# enum of 'STATUS_SCHEDULED, STATUS_HALFTIME'
 		self.shortDetail = shortDetail # String (e.g. '1/25 - ' '10:00 PM ' 'EST')
 		self.state = state # String probably enum (e.g. 'pre')
+		
+		if altDetail is not None:
+			self.altDetail = altDetail
 
 class Ticket():
 	def __init__(self, links, numberAvailable, summary):
--- plusMinusEstimator.py
+++ plusMinusEstimator.py
@@ -13,22 +13,27 @@ from NbaGames import NbaGames
 from pprint import pprint
 
 def getEvents():
-	threading.Timer(30.0, getEvents).start()
+	threading.Timer(5.0, getEvents).start()
 	request = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard'
+#	request = 'https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard'
+#	request = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard'
 	dataRequest = requests.get(request).json()
-	#	pprint(dataRequest)
 
 	nbaGames = NbaGames(**dataRequest)
-
+	
+	count = 0
 	for event in nbaGames.events:
+		count += 1
 		if event.status.type.name != constants.STATUS_SCHEDULED and event.status.type.name != constants.STATUS_POSTPONED:
 			calculateAndPrintData(event)
 		else:
 			print("{} - {}\n".format(event.name, event.status.type.name))
 	print("\n")
-
+	
 def calculateAndPrintData(event):
 	totalProjectedScore = 0
+	goodOverLine = 0
+	goodUnderLine = 0
 	competition = event.competitions[0]
 
 	period = competition.status.period
@@ -50,8 +55,10 @@ def calculateAndPrintData(event):
 		# PointsPerSecond * TotalSeconds
 	if secondsPlayed > 0:
 		totalProjectedScore = (totalCurrentScore / secondsPlayed) * (constants.NUMBER_OF_PERIODS * constants.SECONDS_PER_PERIOD)
+		goodOverLine = totalProjectedScore + ((1 - (secondsPlayed/(constants.NUMBER_OF_PERIODS * constants.SECONDS_PER_PERIOD))) * constants.GOOD_OVER_UNDER)
+		goodUnderLine = totalProjectedScore - ((1 - (secondsPlayed/(constants.NUMBER_OF_PERIODS * constants.SECONDS_PER_PERIOD))) * constants.GOOD_OVER_UNDER)
 
-	print("___________________________\n{}\n{}	{}\n+/- {}\nQ{} - {}\n___________________________".format(event.name, awayTeam, homeTeam, round(totalProjectedScore, 2), period, competition.status.displayClock))
+	print("___________________________\n{}\n{}	{}\nO: - {}\n+/- {}\nU: - {}\nQ{} - {}\n{}\n___________________________".format(event.name, awayTeam, homeTeam, round(goodUnderLine, 2), round(totalProjectedScore, 2), round(goodOverLine, 2), period, competition.status.displayClock, event.status.type.name))
 
 # --------------------------------------------------------------------------------------
 # This program begins by getting all the NBA games that have are currently being played.
